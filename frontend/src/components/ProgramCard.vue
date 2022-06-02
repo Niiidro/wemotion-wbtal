fdb6685588c5542f6aee0896c1ef0ae51ffe9be7
pminder <philipp.minder@hotmail.com>
2022-06-02 21:55:11 +0200

    yx

---
+++ frontend/src/components/ProgramCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-1/2 mb-10">
+    <div
+      class="m-auto text-7xl text-center"
+      :class="[program.image ? 'translate-y-20 text-white' : 'text-wemotion-purple']">
+      {{ program.title }}
+    </div>
+    <div class="h-full w-full flex border border-green-700 shadow-lg">
+      <img
+        v-if="program.image"
+        :src="`https://directus.wemotion-wbtal.ch/assets/${program.image.id}`"
+        class="m-auto p-1 object-contain w-full" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  program: { type: Object, required: true },
+});
+</script>
--- frontend/src/views/Program.vue
+++ frontend/src/views/Program.vue
@@ -2,16 +2,15 @@
   <div v-if="program">
     <div class="mt-5">
       <div class="text-4xl border-b text-wemotion-purple border-green-700">Programm</div>
-      <div class="flex mt-5 gap-10 flex-wrap flex-col">
-        <div v-for="(item, index) in program.program" :key="index">
-          <div class="text-3xl text-green-700 text-center">{{ item.title }}</div>
-        </div>
+      <div class="flex mt-5 gap-10 flex-col">
+        <ProgramCard v-for="(item, index) in program.program" :key="index" :program="item" class="odd:ml-auto" />
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { useQuery } from 'villus';
+import ProgramCard from '../components/ProgramCard.vue';
 
 const { data: program } = useQuery({
   query: `{
