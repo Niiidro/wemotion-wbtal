18a4465d664cfc24494423587adf51a55e5ee40e
pminder <philipp.minder@hotmail.com>
2022-09-22 19:37:56 +0200

    x

--- frontend/src/components/Image.vue
+++ frontend/src/components/Image.vue
@@ -1,7 +1,7 @@
 <template>
   <img
     :src="`https://directus.wemotion-wbtal.ch/assets/${image.directus_files_id.id}`"
-    class="m-auto p-1 object-contain w-80 h-80" />
+    class="p-1 object-contain w-80 h-80" />
 </template>
 
 <script setup lang="ts">
--- frontend/src/views/Gallery.vue
+++ frontend/src/views/Gallery.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex" v-if="gallery">
+  <div class="flex flex-wrap justify-start gap-5" v-if="gallery">
     <Image v-for="(image, index) in gallery.gallery_by_id.images" :key="index" :image="image" />
   </div>
 </template>
