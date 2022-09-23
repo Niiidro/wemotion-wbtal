ec04bdac41e75b61d21503ed17f1a572f2fb9aa4
pminder <philipp.minder@hotmail.com>
2022-09-23 16:43:28 +0200

    x

--- frontend/src/components/Image.vue
+++ frontend/src/components/Image.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    :src="`https://directus.wemotion-wbtal.ch/assets/${image.directus_files_id.id}`"
+    :src="`https://directus.wemotion-wbtal.ch/assets/${image?.directus_files_id.id}`"
     class="p-1 object-contain w-80 h-80" />
 </template>
 
